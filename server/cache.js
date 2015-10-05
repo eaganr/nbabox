0b914b310eaed30f88f31b1bc329d6be2b82be6c
Ryan Eagan <eaganry@gmail.com>
2015-10-05 22:48:51 +0000

    tabs to spaces in cache.js and update.js

--- server/cache.js
+++ server/cache.js
@@ -1,8 +1,8 @@
 /*
 TODO: 
-	- cache for minute, hour, day
-	- cron to move/delete old cache files
-	- cron to autopull live game data to cache
+  - cache for minute, hour, day
+  - cron to move/delete old cache files
+  - cron to autopull live game data to cache
 
 
 
@@ -12,89 +12,89 @@ var folder = "/root/nbabox/"
 
 //time
 function getTime() {
-	var d = new Date();
-	var year = d.getFullYear()+"";
+  var d = new Date();
+  var year = d.getFullYear()+"";
 
-	var month = d.getMonth()+1;
-	if(month<10) {
-		month = "0"+month;
-	}
-	else month = month + "";
+  var month = d.getMonth()+1;
+  if(month<10) {
+    month = "0"+month;
+  }
+  else month = month + "";
 
-	var day = d.getDate();
-	if(day<10) {
-		day = "0"+day;
-	}
-	else day = day + "";
+  var day = d.getDate();
+  if(day<10) {
+    day = "0"+day;
+  }
+  else day = day + "";
 
-	var hour = d.getHours();
-	if(hour<10) {
-		hour = "0"+hour;
-	}
-	else month = month + "";
+  var hour = d.getHours();
+  if(hour<10) {
+    hour = "0"+hour;
+  }
+  else month = month + "";
 
-	var minute = d.getMinutes();
-	if(minute<10) {
-		minute = "0"+minute;
-	}
-	else minute = minute + "";
+  var minute = d.getMinutes();
+  if(minute<10) {
+    minute = "0"+minute;
+  }
+  else minute = minute + "";
 
-	var stamp = ""+year+month+day+hour+minute;
-	return stamp;
+  var stamp = ""+year+month+day+hour+minute;
+  return stamp;
 }
 
 function toTime(dateString) {
-	var year = parseInt(dateString.substring(0,4));
-	var month = parseInt(dateString.substring(4,6))-1;
-	var day = parseInt(dateString.substring(6,8));
-	var hour = parseInt(dateString.substring(8,10));
-	var minute = parseInt(dateString.substring(10,12));
-	return new Date(year,month,day,hour,minute);
+  var year = parseInt(dateString.substring(0,4));
+  var month = parseInt(dateString.substring(4,6))-1;
+  var day = parseInt(dateString.substring(6,8));
+  var hour = parseInt(dateString.substring(8,10));
+  var minute = parseInt(dateString.substring(10,12));
+  return new Date(year,month,day,hour,minute);
 }
 
 //box score
 function requestBoxScore(res, gameID) {
-	var fs = require('fs');
-	var request = require('request');	
-	var url = "http://stats.nba.com/stats/boxscore?StartPeriod=0&EndPeriod=0&StartRange=0&EndRange=0&RangeType=0&GameID="+gameID;
-	request(url, function (error, response, body) {
-		if(!error && response.statusCode == 200) {
-			var response = JSON.parse(body);
+  var fs = require('fs');
+  var request = require('request');  
+  var url = "http://stats.nba.com/stats/boxscore?StartPeriod=0&EndPeriod=0&StartRange=0&EndRange=0&RangeType=0&GameID="+gameID;
+  request(url, function (error, response, body) {
+    if(!error && response.statusCode == 200) {
+      var response = JSON.parse(body);
       fs.writeFile(folder+"cache/minute/boxscore/"+gameID+"boxscore"+getTime()+".json", JSON.stringify(response), function(err2) {
         if(err2) console.log(err2);
         console.log("saved");
         res.jsonp(response);
       });
-		}
-	});
+    }
+  });
 }
 
 //period = 1 - minute, 2 - hour, 3 - day
 function getBoxScore(res, period, gameID) {
-	var j = [];
-	if(period > 0) j = getFile("minute", "boxscore", gameID);
-	if(j.length === 0 && period > 1) j = getFile("hour", "boxscore", gameID);
-	if(j.length === 0 && period > 2) j = getFile("day", "boxscore", gameID);
+  var j = [];
+  if(period > 0) j = getFile("minute", "boxscore", gameID);
+  if(j.length === 0 && period > 1) j = getFile("hour", "boxscore", gameID);
+  if(j.length === 0 && period > 2) j = getFile("day", "boxscore", gameID);
 
-	if(j.length === 0) requestBoxScore(res, gameID);
-	else res.jsonp(j);
+  if(j.length === 0) requestBoxScore(res, gameID);
+  else res.jsonp(j);
 }
 
 //period = minute,hour,day
 function getFile(period, filetype, id) {
-	var fs = require('fs');
-	var j = [];
-	if(filetype === "schedule") id=id.replace(new RegExp("/", 'g'), "-");
+  var fs = require('fs');
+  var j = [];
+  if(filetype === "schedule") id=id.replace(new RegExp("/", 'g'), "-");
 
-	var files = fs.readdirSync(folder+"cache/"+period+"/"+filetype);
-	for(var i=0;i<files.length;i++) {
-		var f = files[i];
-		if(f.indexOf(id+filetype) > -1) {
-			j = require(folder+"cache/"+period+"/"+filetype+"/"+f);
-			break;
-		}
-	}
-	return j;
+  var files = fs.readdirSync(folder+"cache/"+period+"/"+filetype);
+  for(var i=0;i<files.length;i++) {
+    var f = files[i];
+    if(f.indexOf(id+filetype) > -1) {
+      j = require(folder+"cache/"+period+"/"+filetype+"/"+f);
+      break;
+    }
+  }
+  return j;
 }
 
 
@@ -114,44 +114,44 @@ function requestPlayByPlay(res, gameID) {
 
 //period = 1 - minute, 2 - hour, 3 - day
 function getPlayByPlay(res, period, gameID) {
-	var j = [];
-	if(period > 0) j = getFile("minute", "playbyplay", gameID);
-	if(j.length === 0 && period > 1) j = getFile("hour", "playbyplay", gameID);
-	if(j.length === 0 && period > 2) j = getFile("day", "playbyplay", gameID);
+  var j = [];
+  if(period > 0) j = getFile("minute", "playbyplay", gameID);
+  if(j.length === 0 && period > 1) j = getFile("hour", "playbyplay", gameID);
+  if(j.length === 0 && period > 2) j = getFile("day", "playbyplay", gameID);
 
-	if(j.length === 0) requestPlayByPlay(res, gameID);
-	else res.jsonp(j);
+  if(j.length === 0) requestPlayByPlay(res, gameID);
+  else res.jsonp(j);
 }
 
 //schedule
 function requestSchedule(res, date) {
-	var fs = require('fs');
-	var request = require('request');	
-	var url = "http://stats.nba.com/stats/scoreboard/?callback=?&LeagueID=00&DayOffset=0&GameDate="+date;
-	request(url, function (error, response, body) {
-		if(!error && response.statusCode == 200) {
-			body = body.substring(2,body.length-1);
-			var response = JSON.parse(body);
-			date=date.replace(new RegExp("/", 'g'), "-");
+  var fs = require('fs');
+  var request = require('request');  
+  var url = "http://stats.nba.com/stats/scoreboard/?callback=?&LeagueID=00&DayOffset=0&GameDate="+date;
+  request(url, function (error, response, body) {
+    if(!error && response.statusCode == 200) {
+      body = body.substring(2,body.length-1);
+      var response = JSON.parse(body);
+      date=date.replace(new RegExp("/", 'g'), "-");
       if(date.length === 9) date = date.substring(0,3)+"0"+date.substring(3);
-			fs.writeFile(folder+"cache/minute/schedule/"+date+"schedule"+getTime()+".json", JSON.stringify(response), function(err2) {
-				if(err2) console.log(err2);
-				console.log("saved");
-				res.jsonp(response);
-			});
-		}
-	});
+      fs.writeFile(folder+"cache/minute/schedule/"+date+"schedule"+getTime()+".json", JSON.stringify(response), function(err2) {
+        if(err2) console.log(err2);
+        console.log("saved");
+        res.jsonp(response);
+      });
+    }
+  });
 }
 
 //period = 1 - minute, 2 - hour, 3 - day
 function getSchedule(res, period, date) {
-	var j = [];
-	if(period > 0) j = getFile("minute", "schedule", date);
-	if(j.length === 0 && period > 1) j = getFile("hour", "schedule", date);
-	if(j.length === 0 && period > 2) j = getFile("day", "schedule", date);
+  var j = [];
+  if(period > 0) j = getFile("minute", "schedule", date);
+  if(j.length === 0 && period > 1) j = getFile("hour", "schedule", date);
+  if(j.length === 0 && period > 2) j = getFile("day", "schedule", date);
 
-	if(j.length === 0) requestSchedule(res, date);
-	else res.jsonp(j);
+  if(j.length === 0) requestSchedule(res, date);
+  else res.jsonp(j);
 }
 
 //requestBoxScore("0041400406");
@@ -159,21 +159,21 @@ function getSchedule(res, period, date) {
 
 //port listen, communicate with frontend
 (function() {
-	var express=require('express');
-	var app=express();
-	var bodyParser = require('body-parser');
-	app.use(bodyParser.urlencoded({extended:false}));	
+  var express=require('express');
+  var app=express();
+  var bodyParser = require('body-parser');
+  app.use(bodyParser.urlencoded({extended:false}));  
 
-	var server=app.listen(3000,function(){
-		console.log("We have started our server on port 3000");
-	});
+  var server=app.listen(3000,function(){
+    console.log("We have started our server on port 3000");
+  });
 
   app.post('/',function(req,res){
-		console.log(req.body);
-		res.header("Access-Control-Allow-Origin", "*");
-		if(req.body.func === "getBoxScore") getBoxScore(res,3,req.body.gameID);
-		if(req.body.func === "getPlayByPlay") getPlayByPlay(res,3,req.body.gameID);
-		if(req.body.func === "getSchedule") getSchedule(res,3,req.body.date);
-	});
+    console.log(req.body);
+    res.header("Access-Control-Allow-Origin", "*");
+    if(req.body.func === "getBoxScore") getBoxScore(res,3,req.body.gameID);
+    if(req.body.func === "getPlayByPlay") getPlayByPlay(res,3,req.body.gameID);
+    if(req.body.func === "getSchedule") getSchedule(res,3,req.body.date);
+  });
 }).call(this);
 
--- server/update.js
+++ server/update.js
@@ -2,28 +2,28 @@ var folder = "/root/nbabox/"
 
 
 function getTimeStamp(filename, filetype) {
-	var stamp = filename.substring(10+filetype.length, filename.length-5);
-	return stamp;
+  var stamp = filename.substring(10+filetype.length, filename.length-5);
+  return stamp;
 }
 
 
 function toTime(dateString) {
-	var year = parseInt(dateString.substring(0,4));
-	var month = parseInt(dateString.substring(4,6))-1;
-	var day = parseInt(dateString.substring(6,8));
-	var hour = parseInt(dateString.substring(8,10));
-	var minute = parseInt(dateString.substring(10,12));
-	return new Date(year,month,day,hour,minute);
+  var year = parseInt(dateString.substring(0,4));
+  var month = parseInt(dateString.substring(4,6))-1;
+  var day = parseInt(dateString.substring(6,8));
+  var hour = parseInt(dateString.substring(8,10));
+  var minute = parseInt(dateString.substring(10,12));
+  return new Date(year,month,day,hour,minute);
 }
 
 
 function updateCache() {
-	var fs = require('fs');
+  var fs = require('fs');
 
-	//minute check
+  //minute check
   var types = ["playbyplay", "boxscore", "schedule"];
   for(var n=0;n<types.length;n++) {
-	  var files = fs.readdirSync(folder+"cache/minute/"+types[n]);
+    var files = fs.readdirSync(folder+"cache/minute/"+types[n]);
     for(var i=0;i<files.length;i++) {
       var filename = files[i];
       var fileDate = toTime(getTimeStamp(filename, types[n]));
@@ -58,24 +58,24 @@ function updateCache() {
       }
     }
   }
-	/*
-	//day check
-	var files = fs.readdirSync(folder+"cache/day");
-	for(var i=0;i<files.length;i++) {
-		var filename = files[i];
-		var fileDate = toTime(getTimeStamp(filename));
-		var thisDay = new Date();
-		thisDay.setHours(0);
-		if(thisDay - fileDate > 86400000) {
-			//move file to hour folder
-			fs.unlink(folder+"cache/day/"+filename,
-				      function(err) {
-				      	if(err) console.log(err);
-				      }
-				     );
-		}
-	}
-	*/
+  /*
+  //day check
+  var files = fs.readdirSync(folder+"cache/day");
+  for(var i=0;i<files.length;i++) {
+    var filename = files[i];
+    var fileDate = toTime(getTimeStamp(filename));
+    var thisDay = new Date();
+    thisDay.setHours(0);
+    if(thisDay - fileDate > 86400000) {
+      //move file to hour folder
+      fs.unlink(folder+"cache/day/"+filename,
+              function(err) {
+                if(err) console.log(err);
+              }
+             );
+    }
+  }
+  */
 }
 
 
